6b592d4ef6cdb6e9404fb5af05aabcaa88e1a44f
chinmoy07-an <chinmoy.cool36@gmail.com>
2020-03-22 22:33:34 +0530

    Included Hidden files count and Input Sanitation

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 function guess(){
-    correct_ans=$(ls -1|wc -l)
+    correct_ans=$(ls -1a|wc -l)  #To show count including hidden files or .files
     while true;
     do
         echo "Put in your guess"
         read  your_guess
-        if [ $your_guess -lt $correct_ans ]
+        if ! [[ "$your_guess" =~ ^[0-9]+$ ]]
+        then
+             echo "Sorry integers only"
+        elif [ $your_guess -lt $correct_ans ]
         then
             echo "guess is Less then the true answer"
         elif [ $your_guess -gt $correct_ans ]
